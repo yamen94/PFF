f7e713702e4772ccdf9a8963f98b0fdf56c437f9
yegebril <yamenemad4@gmail.com>
2018-09-07 23:00:37 +0200

    home_fragment

---
+++ app/src/main/java/com/gebril/yamen/pff/activities/tools/Preferences.java
@@ -0,0 +1,16 @@
+package com.gebril.yamen.pff.activities.tools;
+
+/**
+ * Created by yegebril on 6/30/2018.
+ */
+
+public class Preferences {
+
+   public static final String  LOGGED_IN = "first_log";
+   public static final String  FIRST_TIME = "first_time";
+   public static final String  REMEMBER_ME = "remember_me";
+   public static final int ALREADY_EXIST = 0;
+   public static final int ERROR = -1;
+   public static final int IS_VALID = 1;
+   public static final String BASE_URL = "http://192.168.8.101/PFF/v1/";
+}
