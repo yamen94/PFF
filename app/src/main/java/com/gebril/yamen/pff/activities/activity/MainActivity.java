e4e24956b9a37438e6da26ff3d93541eb9efe5ab
yegebril <yamenemad4@gmail.com>
2018-10-23 16:00:41 +0200

    Navigation_Drawer

--- app/src/main/java/com/gebril/yamen/pff/activities/activity/MainActivity.java
+++ app/src/main/java/com/gebril/yamen/pff/activities/activity/MainActivity.java
@@ -140,17 +140,20 @@ public class MainActivity extends AppCompatActivity   {
                 // Handle navigation view item clicks here.
                 int id = item.getItemId();
 
-                if (id == R.id.nav_camera) {
-                    // Handle the camera action
-                } else if (id == R.id.nav_gallery) {
+                if (id == R.id.nav_bio) {
+                    // Handle the bio action
+                } else if (id == R.id.nav_my_teams) {
 
-                } else if (id == R.id.nav_slideshow) {
+                } else if (id == R.id.nav_my_items) {
 
-                } else if (id == R.id.nav_manage) {
+                } else if (id == R.id.nav_block) {
 
-                } else if (id == R.id.nav_share) {
+                } else if (id == R.id.nav_settings) {
 
-                } else if (id == R.id.nav_send) {
+                } else if (id == R.id.nav_logout) {
+
+                }
+                else if (id == R.id.nav_help) {
 
                 }
 
